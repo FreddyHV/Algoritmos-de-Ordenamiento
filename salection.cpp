9f303feb2b3a0c11d2f8ed966784936e8a75627f
FreddyHV <65179010+FreddyHV@users.noreply.github.com>
2020-05-13 22:19:20 -0500

    Add files via upload

---
+++ salection.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main(){
+    int i,j,n,loc,temp,min,a[30];
+    cout<<"ingrese la cantidad de elementos:";
+    cin>>n;
+    cout<<"\nIngrese numeros\n";
+ 
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n-1;i++){
+        min=a[i];
+        loc=i;
+        for(j=i+1;j<n;j++){
+            if(min>a[j]){
+                min=a[j];
+                loc=j;
+            }
+        }
+        temp=a[i];
+        a[i]=a[loc];
+        a[loc]=temp;
+    }
+    cout<<"\nLa lista de numeros ordenada es:\n";
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
